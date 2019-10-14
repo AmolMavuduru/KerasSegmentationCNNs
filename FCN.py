bc94b89eadba96459328c56b6bb47599d2a1278b
AmolMavuduru <rovingrook09@yahoo.com>
2019-10-14 15:45:13 -0500

    Added several segmentation CNNs and implementations

---
+++ FCN.py
@@ -0,0 +1,11 @@
+"""
+Implementations of fully convolutional networks (FCNs) from classic architectures such as:
+- AlexNet
+- VGG16
+- ResNet
+"""
+
+from keras.layers import *
+from keras.models import Sequential
+from keras.applications import vgg16
+
